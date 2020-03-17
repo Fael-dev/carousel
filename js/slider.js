25075b05c7688948e502748a698f6a6beefb8836
rafael <rafael@MacBook-de-FIXPAY.local>
2020-03-17 10:34:02 -0700

    Carousel funcionando

--- js/slider.js
+++ js/slider.js
@@ -2,18 +2,30 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
+  var time = 5000;
   var i;
+  // Pegando as classe mySlides e dot dentro de variáveis
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
+
+  // Loop deixa todos os elementos com mySlides com display none
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";  
   }
+
+  // Incrementando +1 na variável slideIndex 
   slideIndex++;
+  
+  // Se o index do slide, for maior que o número total de slides, o index será 1
   if (slideIndex > slides.length) {slideIndex = 1}    
+ 
+  // Loop que adiciona os dots 
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
+  // Faz o slide funcionar 
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  setTimeout(showSlides, time); // Passando slides em 1 segundo
 }
