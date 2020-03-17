4e83555b65145250d8485551be471803dce1ea96
rafael <rafael@MacBook-de-FIXPAY.local>
2020-03-17 09:11:23 -0700

    Slide automatico funcionando, faltando colocar tela cheia

--- js/slider.js
+++ js/slider.js
@@ -1,38 +1,19 @@
-/*
-    Pega todos os elementos li da lista não ordenada
-    retornando um objeto NodeList, 
-    de acordo com posicionamento encontrado
-    no documento
- 
-*/
-var sliders  = document.querySelectorAll('#sliders li');
-/*
- Define o indice, que será responsavel 
- por informar os slider atual
- */
-var currentPositionSlide  = 0;
-/*
- Retorna a último posição do slide.
- Diminui menos 1, devido o primeiro
- item começa com zero. 
- Se temos 2 items, sua posição:
- 0,1,2
-*/
-var endPositionSlide    = sliders.length - 1;
-/*
-  Executaremos uma função num intervalo de tempo.
-  E defiremos que executará em 2000 milesegundos
-*/
-window.setInterval(function(){
-    /*
-     a variavel index será responsável pela
-     posição do elemento que iremos remover a classe
-    */
-    var lastPositionSlide =  currentPositionSlide  ? currentPositionSlide  - 1 : endPositionSlide;
-    /*Pega o elemento pra remover a classe*/
-    sliders[lastPositionSlide].className  = '';
-    /*Adiciona a classe no elemento atual*/
-    sliders[currentPositionSlide].className  = 'slider-active';
-    /*Calcular a posição do próximo elemento que será exibido*/
-    currentPositionSlide= currentPositionSlide >= endPositionSlide? 0 : currentPositionSlide+1;  
-}, 2000);
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
